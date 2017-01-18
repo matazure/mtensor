447f95b0892696b7b24c5aacb5c69cbd0739567b
Zhang Zhimin <p3.1415@qq.com>
2017-01-18 20:41:57 +0800

    add cuda switch for benchmark

--- benchmark/tensor_benchmark.cu
+++ benchmark/tensor_benchmark.cu
@@ -19,14 +19,18 @@ static void BM_mem_copy(benchmark::State& state) {
 }
 
 auto BM_host2host_mem_copy = BM_mem_copy<tensor<byte, 1>, tensor<byte, 1>>;
+#ifdef MATAZURE_CUDA
 auto BM_device2host_mem_copy = BM_mem_copy<cu_tensor<byte, 1>, tensor<byte, 1>>;
 auto BM_host2device_mem_copy = BM_mem_copy<tensor<byte, 1>, cu_tensor<byte, 1>>;
 auto BM_device2device_mem_copy = BM_mem_copy<cu_tensor<byte, 1>, cu_tensor<byte, 1>>;
+#endif
 
 BENCHMARK(BM_host2host_mem_copy)->Range(1, 1 << 30)->UseRealTime();
+#ifdef MATAZURE_CUDA
 BENCHMARK(BM_device2host_mem_copy)->Range(1, 1 << 30)->UseRealTime();
 BENCHMARK(BM_host2device_mem_copy)->Range(1, 1 << 30)->UseRealTime();
 BENCHMARK(BM_device2device_mem_copy)->Range(1, 1 << 30)->UseRealTime();
+#endif
 
 BENCHMARK_MAIN()
 
