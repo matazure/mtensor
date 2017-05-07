1325e12983a15aa0f2f27e72eb9b66b9729a5bd2
Zhang Zhimin <p3.1415@qq.com>
2017-05-08 01:07:13 +0800

    add prod block aligned

--- matazure/cuda/numeric/prod.hpp
+++ matazure/cuda/numeric/prod.hpp
@@ -4,66 +4,120 @@
 
 namespace matazure {
 namespace cuda {
+namespace numeric {
 
-template <typename _Tensor1, typename _Tensor2>
-struct __product {
-	typedef typename _Tensor1::value_type result_type;
+template <int_t _BlockSize, typename _MatrixLhs, typename _MatrixRhs, typename _MatrixRe>
+inline void prod_block_aligned(_MatrixLhs cmat_lhs, _MatrixRhs cmat_rhs, _MatrixRe &cmat_re) {
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixLhs);
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixRhs);
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixRe);
+	MATAZURE_STATIC_ASSERT_VALUE_TYPE_MATCHED(_MatrixLhs, _MatrixRhs);
+	MATAZURE_STATIC_ASSERT_VALUE_TYPE_MATCHED(_MatrixRhs, _MatrixRe);
+	MATAZURE_ASSERT(equal(cmat_re.shape(), pointi<2>{cmat_lhs.shape()[0], cmat_rhs.shape()[1]}), "unmatched shape");
 
-	__product(_Tensor1 cmat_lhs, _Tensor2 cmat_rhs) : cmat_lhs_(cmat_lhs), cmat_rhs_(cmat_rhs) {}
+	typedef typename _MatrixRe::value_type value_type;
 
-	MATAZURE_DEVICE result_type operator()(pointi<2> idx) const {
-		result_type re = 0;
-		for (int_t i = 0; i < cmat_rhs_.shape()[1]; ++i) {
-			re += cmat_lhs_({ i, idx[1] }) * cmat_rhs_({ idx[0], i });
-		}
-		return re;
-	}
+	constexpr auto block_size = _BlockSize;
+	auto grid_ext = cmat_re.shape() / block_size;
+	MATAZURE_ASSERT(equal(grid_ext * block_size, cmat_re.shape()), "unaligned shape");
 
-private:
-	_Tensor1 cmat_lhs_;
-	_Tensor2 cmat_rhs_;
-};
-
-template <typename _Tensor1, typename _Tensor2>
-device_lambda_tensor<typename _Tensor1::value_type, array_access_t, 2, __product<_Tensor1, _Tensor2>> product(_Tensor1 cmat_lhs, _Tensor2 cmat_rhs) {
-	return make_lambda<typename _Tensor1::value_type, array_access_t>(pointi<2>{cmat_rhs.shape()[0], cmat_lhs.shape()[1]}, __product<_Tensor1, _Tensor2>(cmat_lhs, cmat_rhs));
-}
-
-template <int_t _BlockSize, typename _Matrix1, typename _Matrix2>
-matrix<typename _Matrix1::value_type>  block_product(_Matrix1 cmat_lhs, _Matrix2 cmat_rhs) {
-	typedef typename _Matrix1::value_type value_type;
-	auto lhs_ext = cmat_lhs.shape();
-	auto rhs_ext = cmat_rhs.shape();
-	matrix<value_type> cmat_re({ lhs_ext[0], rhs_ext[1] });
-	pointi<2> block_ext{ _BlockSize, _BlockSize };
-
-	block_for_index<_BlockSize, _BlockSize>(cmat_re.shape() / block_ext, [=] MATAZURE_DEVICE(block_index<_BlockSize, _BlockSize> t_idx) {
+	block_for_index<dim<block_size, block_size>>(grid_ext, [=] MATAZURE_DEVICE(block_index<dim<block_size, block_size>> t_idx) {
 		auto row = t_idx.local[0];
 		auto col = t_idx.local[1];
 		auto global_row = t_idx.global[0];
 		auto global_col = t_idx.global[1];
 
-		__shared__ static_tensor<value_type,dim< _BlockSize,  _BlockSize>> local_lhs;
-		__shared__ static_tensor<value_type,dim< _BlockSize,  _BlockSize>> local_rhs;
+		__shared__ static_tensor<value_type, dim<block_size, block_size>> local_lhs;
+		__shared__ static_tensor<value_type, dim<block_size, block_size>> local_rhs;
 
-		value_type sum = 0;
-		for (int_t i = 0; i < lhs_ext[0]; i += _BlockSize) {
-			local_lhs({ row, col }) = cmat_lhs({ global_row, col + i });
-			local_rhs({ col, row }) = cmat_rhs({ row+i, global_col });
-			block_barrier();
+		auto sum = zero<value_type>::value();
+		for (int_t i = 0; i < cmat_lhs.shape()[1]; i += block_size) {
+			local_lhs(row, col) = cmat_lhs(global_row, col + i);
+			local_rhs(row, col) = cmat_rhs(row + i, global_col);
+			device::barrier();
 
-			for (int_t k = 0; k < _BlockSize; k++) {
-				sum += local_lhs({ row, k }) *local_rhs({ k, col });
+			for (int_t k = 0; k < block_size; k++) {
+				sum += local_lhs(row, k) * local_rhs(k, col);
 			}
 
-			block_barrier();
+			device::barrier();
 		}
 
-		cmat_re(t_idx.global) = sum;
+		cmat_re[t_idx.global] = sum;
 	});
+}
 
+template <int_t _BlockSize, typename _MatrixLhs, typename _MatrixRhs>
+inline auto prod_block_aligned(_MatrixLhs cmat_lhs, _MatrixRhs cmat_rhs)->matrix<typename _MatrixLhs::value_type> {
+	matrix<typename _MatrixLhs::value_type> cmat_re(cmat_lhs.shape()[0], cmat_rhs.shape()[1]);
+	prod_block_aligned<_BlockSize>(cmat_lhs, cmat_rhs, cmat_re);
 	return cmat_re;
 }
 
+template <int_t _BlockSize, typename _MatrixLhs, typename _MatrixRhs, typename _MatrixRe>
+inline void prod_block(_MatrixLhs cmat_lhs, _MatrixRhs cmat_rhs, _MatrixRe &cmat_re) {
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixLhs);
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixRhs);
+	MATAZURE_STATIC_ASSERT_MATRIX_RANK(_MatrixRe);
+	MATAZURE_STATIC_ASSERT_VALUE_TYPE_MATCHED(_MatrixLhs, _MatrixRhs);
+	MATAZURE_STATIC_ASSERT_VALUE_TYPE_MATCHED(_MatrixRhs, _MatrixRe);
+	MATAZURE_ASSERT(equal(cmat_re.shape(), pointi<2>{cmat_lhs.shape()[0], cmat_rhs.shape()[1]}), "unmatched shape");
+
+	typedef typename _MatrixRe::value_type value_type;
+
+	constexpr auto block_size = _BlockSize;
+	auto grid_ext = (cmat_re.shape() + block_size - 1) / block_size;
+
+	block_for_index<dim<_BlockSize, _BlockSize>>(grid_ext, [=] MATAZURE_DEVICE (block_index<dim<_BlockSize, _BlockSize>> block_idx) {
+		auto row = block_idx.local[0];
+		auto col = block_idx.local[1];
+		auto global_row = block_idx.global[0];
+		auto global_col = block_idx.global[1];
+
+		__shared__ static_tensor<value_type, dim<block_size, block_size>> local_lhs;
+		__shared__ static_tensor<value_type, dim<block_size, block_size>> local_rhs;
+
+		auto is_valid = inside(block_idx.global, pointi<2>::zeros(), cmat_re.shape());
+		auto l = cmat_lhs.shape()[0];
+		auto m = cmat_lhs.shape()[1];
+		auto n = cmat_rhs.shape()[1];
+		auto sum = zero<value_type>::value();
+		for (int_t i = 0; i < m; i += block_size) {
+			if (global_row < l && col + i < m) {
+				local_lhs(row, col) = cmat_lhs(global_row, col + i);
+			}
+			else {
+				local_lhs(row, col) = zero<value_type>::value();
+			}
+			if (global_col < n, row + i < m) {
+				local_rhs(row, col) = cmat_rhs(row + i, global_col);
+			}
+			else {
+				local_rhs(row, col) = zero<value_type>::value();
+			}
+	
+			device::barrier();
+
+			if (is_valid) {
+				for (int_t k = 0; k < block_size; k++) {
+					sum += local_lhs(row, k) * local_rhs(k, col);
+				}
+			}
+			device::barrier();
+		}
+
+		if (is_valid)
+			cmat_re[block_idx.global] = sum;
+	});
+}
+
+template <int_t _BlockSize, typename _MatrixLhs, typename _MatrixRhs>
+inline auto prod_block(_MatrixLhs cmat_lhs, _MatrixRhs cmat_rhs)->matrix<typename _MatrixLhs::value_type> {
+	matrix<typename _MatrixLhs::value_type> cmat_re(cmat_lhs.shape()[0], cmat_rhs.shape()[1]);
+	prod_block<_BlockSize>(cmat_lhs, cmat_rhs, cmat_re);
+	return cmat_re;
+}
+
+}
 }
 }
--- matazure/meta.hpp
+++ matazure/meta.hpp
@@ -9,16 +9,23 @@ namespace meta {
 template <int_t _V>
 using int_t_c = std::integral_constant<int_t, _V>;
 
-template <int_t ..._Values0>
+template <int_t ..._Values>
 struct array{
+	MATAZURE_GENERAL constexpr array():
+		elements_{_Values ...}
+	{}
+
 	MATAZURE_GENERAL static constexpr int_t size() {
-		return sizeof...(_Values0);
+		return sizeof...(_Values);
 	}
+
+	int_t elements_[sizeof...(_Values)];
 };
 
 template <int_t _Idx, int_t ..._Values>
 inline MATAZURE_GENERAL constexpr int_t at_c(array<_Values...> a) {
-	return std::array<int_t, sizeof...(_Values)>{_Values...}[_Idx];
+	static_assert(_Idx < sizeof...(_Values), "_Idx should be less than meta array size");
+	return a.elements_[_Idx];
 }
 
 #define MATAZURE_META_BINARY_FUNC(name, op)																						\
@@ -36,6 +43,9 @@ template <int_t _V, int_t ..._Values>																							\
 inline MATAZURE_GENERAL constexpr auto name##_c(int_t_c<_V>, array<_Values ...>)->array<_V op _Values ...> {					\
 	return array<_V op _Values ...>{};																							\
 }
+
+template <int_t _V0, int_t _V1>
+inline MATAZURE_GENERAL constexpr auto transpose(array<_V0, _V1>)->array<_V1, _V0>{ return array<_V1, _V0>{}; }
 
 MATAZURE_META_BINARY_FUNC(add, +)
 MATAZURE_META_BINARY_FUNC(sub, -)
