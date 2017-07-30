df09f36c35246e8ee8a0f6963dffe650eba6d98f
Zhang Zhimin <zhimin.zhang@ninebot.com>
2017-07-31 01:40:06 +0800

    serialization

---
+++ matazure/serialization.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace matazure { namespace  serialization {
+
+	class binary_archive{
+
+	};
+
+	class binary_stream{
+
+	};
+
+	class info_archive{
+		//文本+二进制
+	};
+
+	// class text_archive{
+	//
+	// };
+
+} }
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,9 @@
-
+#include <gtest/gtest.hpp>
 
 int main(int argc, char *argv[]){
+
+
+
 
 	return 0;
 }
