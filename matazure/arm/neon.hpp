6918b8f5730ac360017ca47410ba5ec913bbe1e0
Zhang Zhimin <p3.1415@qq.com>
2017-07-30 13:24:48 +0800

    add packet128

--- matazure/arm/neon.hpp
+++ matazure/arm/neon.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+
+namespace matazure { namespace arm {
+
+	template <typename _ValueType>
+	struct packet128{
+
+	};
+
+	using packetf4 = packet128<float>;
+	using packeti4 = packet128<int>;
+
+} }
