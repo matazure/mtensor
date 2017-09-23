61327115e2967d4d17c3ae7705693c35b143d9fa
Zhimin Zhang <zhimin.zhang@ninebot.com>
2017-09-23 19:01:45 +0800

    remove icc compile script

--- script/build_icc.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p build_icc
-cd build_icc
-CXX=icc cmake .. -DCMAKE_BUILD_TYPE=Release
-
-if [ "$(uname)" = 'Darwin' ]; then
-    cmake --build . -- "-j$(sysctl -n hw.ncpu)" || exit 1
-else
-    cmake --build . -- "-j$(nproc)" || exit 1
-fi
