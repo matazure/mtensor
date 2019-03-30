7f5d1875ab3c06874234a379d2ea28d81c5912f0
Zhang Zhimin <p3.1415@qq.com>
2019-03-30 13:26:15 +0800

    fix ci build

--- script/build.sh
+++ script/build.sh
@@ -2,9 +2,4 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"  $@
 
-# Cross-platform parallel build
-if [ "$(uname)" = 'Darwin' ]; then
-    cmake --build . -- "-j$(sysctl -n hw.ncpu)" || exit 1
-else
-    cmake --build . -- "-j$(nproc)" || exit 1
-fi
+cmake --build . -- -j|| exit 1
--- script/build_android.sh
+++ script/build_android.sh
@@ -33,9 +33,5 @@ cmake .. \
     $@ \
     || exit 1
 
-# Cross-platform parallel build
-if [ "$(uname)" = 'Darwin' ]; then
-    cmake --build . -- "-j$(sysctl -n hw.ncpu)" || exit 1
-else
-    cmake --build . -- "-j$(nproc)" || exit 1
-fi
+
+cmake --build . -- -j || exit 1
