35426a6ae9e612a295b5684af3ccac339fa0f9da
Zhang Zhimin <p3.1415@qq.com>
2020-01-14 14:01:16 +0800

    add clang-format

--- sample/sample6_convolution.cu
+++ sample/sample6_convolution.cu
@@ -33,7 +33,7 @@ int main(int argc, char * argv[]) {
 
 	cuda::tensor<pointf<3>, 2> cimg_mean(img_rgb.shape());
 
-	cuda::block_for_index<BLOCK_SIZE>(grid_dim, [=] __device__ (cuda::block_index<BLOCK_SIZE> block_idx){
+	cuda::block_for_index<BLOCK_SIZE>(grid_dim, [=] __device__ (cuda::block_index<BLOCK_SIZE> block_idx) {
 		auto valid_global_idx = valid_block_dim * block_idx.block + block_idx.local - padding;
 		__shared__ static_tensor<pointf<3>, BLOCK_SIZE> sh_ts_block;
 
@@ -43,10 +43,9 @@ int main(int argc, char * argv[]) {
 
 		cuda::sync_threads();
 
-		if (inside_range(block_idx.local, padding, block_idx.block_dim - padding)
-			&& inside_range(valid_global_idx, pointi<2>::zeros(), cimg_padding.shape())) {
+		if (inside_range(block_idx.local, padding, block_idx.block_dim - padding) && inside_range(valid_global_idx, pointi<2>::zeros(), cimg_padding.shape())) {
 			auto sum = zero<pointf<3>>::value();
-			for_index(pointi<2>::zeros(), ckernel_mean.shape(), [&](const pointi<2> &idx) {
+			for_index(pointi<2>::zeros(), ckernel_mean.shape(), [&](const pointi<2> & idx) {
 				sum += sh_ts_block(block_idx.local + idx - padding) * ckernel_mean(idx);
 			});
 			cimg_mean[valid_global_idx] = sum;
