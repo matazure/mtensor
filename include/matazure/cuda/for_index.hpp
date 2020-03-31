da8057849b6cf6f4d03ea876e5dc53ac6df4fd2b
Zhang Zhimin <p3.1415@qq.com>
2020-04-01 01:58:59 +0800

    add lauch.hpp

--- include/matazure/cuda/for_index.hpp
+++ include/matazure/cuda/for_index.hpp
@@ -1,31 +1,12 @@
 #pragma once
 
 #include <matazure/cuda/execution.hpp>
+#include <matazure/cuda/launch.hpp>
 #include <matazure/cuda/tensor.hpp>
 #include <matazure/point.hpp>
 
 namespace matazure {
 namespace cuda {
-
-#pragma nv_exec_check_disable
-template <typename Function, typename... Arguments>
-MATAZURE_GLOBAL void kenel(Function f, Arguments... args) {
-    f(args...);
-}
-
-template <typename _ExecutionPolicy, typename _Fun, typename... _Args>
-inline void launch(_ExecutionPolicy exe_policy, _Fun f, _Args... args) {
-    configure_grid(exe_policy, kenel<_Fun, _Args...>);
-    kenel<<<exe_policy.grid_size(), exe_policy.block_dim(), exe_policy.shared_mem_bytes(),
-            exe_policy.stream()>>>(f, args...);
-    assert_runtime_success(cudaGetLastError());
-}
-
-template <typename _Fun, typename... _Args>
-inline void launch(_Fun f, _Args... args) {
-    execution_policy exe_policy;
-    launch(exe_policy, f, args...);
-}
 
 namespace internal {
 
@@ -99,5 +80,5 @@ template <int_t _Rank, typename _Fun>
 inline void for_index(pointi<_Rank> end, _Fun fun) {
     cuda::for_index(zero<pointi<_Rank>>::value(), end, fun);
 }
-}
+}  // namespace cuda
 }  // namespace matazure
---
+++ include/matazure/cuda/launch.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <matazure/cuda/execution.hpp>
+#include <matazure/cuda/launch.hpp>
+#include <matazure/cuda/tensor.hpp>
+#include <matazure/point.hpp>
+
+namespace matazure {
+namespace cuda {
+
+template <typename Function, typename... Arguments>
+MATAZURE_GLOBAL void kenel(Function f, Arguments... args) {
+    f(args...);
+}
+
+template <typename _ExecutionPolicy, typename _Fun, typename... _Args>
+inline void launch(_ExecutionPolicy exe_policy, _Fun f, _Args... args) {
+    configure_grid(exe_policy, kenel<_Fun, _Args...>);
+    kenel<<<exe_policy.grid_size(), exe_policy.block_dim(), exe_policy.shared_mem_bytes(),
+            exe_policy.stream()>>>(f, args...);
+    assert_runtime_success(cudaGetLastError());
+}
+
+template <typename _Fun, typename... _Args>
+inline void launch(_Fun f, _Args... args) {
+    execution_policy exe_policy;
+    launch(exe_policy, f, args...);
+}
+
+}  // namespace cuda
+}  // namespace matazure
