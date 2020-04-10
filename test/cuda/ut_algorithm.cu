fab7fca95f0d02b2d1e9ba06269e02e678de85d6
Zhang Zhimin <p3.1415@qq.com>
2020-04-10 14:10:22 +0800

    add ut_launch.cu

--- test/cuda/ut_algorithm.cu
+++ test/cuda/ut_algorithm.cu
@@ -1,35 +1,35 @@
-#include <gtest/gtest.h>
-#include <matazure/cuda/lambda_tensor.hpp>
-#include <mtensor.hpp>
-// #include <nvfunctional>
+// #include <gtest/gtest.h>
+// #include <matazure/cuda/lambda_tensor.hpp>
+// #include <mtensor.hpp>
+// // #include <nvfunctional>
 
-using namespace matazure;
-using namespace testing;
+// using namespace matazure;
+// using namespace testing;
 
-__device__ void print(int i) { printf("%d,", i); }
+// __device__ void print(int i) { printf("%d,", i); }
 
-struct print_op {
-    __matazure__ void operator()(int i) { printf("%d, ", i); }
-};
+// struct print_op {
+//     __matazure__ void operator()(int i) { printf("%d, ", i); }
+// };
 
-// #pragma nv_exec_check_disable
-// __matazure__ void printhd(int i) { print(i); }
+// // #pragma nv_exec_check_disable
+// // __matazure__ void printhd(int i) { print(i); }
 
-TEST(LauchTest, OnlySupportDeviceLambda) {
-    cuda::launch([] __device__() { /*printf("thread x: %d\n", threadIdx.x);*/ });
-}
+// TEST(LauchTest, OnlySupportDeviceLambda) {
+//     cuda::launch([] __device__() { /*printf("thread x: %d\n", threadIdx.x);*/ });
+// }
 
-TEST(ForIndexTest, Lambda) {
-    cuda::for_index(0, 10, [] __matazure__(int i) { printf("%d,", i); });
-    printf("\n");
+// TEST(ForIndexTest, Lambda) {
+//     cuda::for_index(0, 10, [] __matazure__(int i) { printf("%d,", i); });
+//     printf("\n");
 
-    cuda::for_index(0, 10, [] __device__(int i) { printf("%d,", i); });
-    printf("\n");
+//     cuda::for_index(0, 10, [] __device__(int i) { printf("%d,", i); });
+//     printf("\n");
 
-    // if use device code in host, will do none
-    matazure::for_index(0, 10, [] __device__(int i) { printf("%d,", i); });
-    printf("\n");
+//     // if use device code in host, will do none
+//     matazure::for_index(0, 10, [] __device__(int i) { printf("%d,", i); });
+//     printf("\n");
 
-    // cuda::for_index(0, 10, print_op{});
-    // printf("\n");
-}
+//     // cuda::for_index(0, 10, print_op{});
+//     // printf("\n");
+// }
--- test/cuda/ut_lambda_tensor.cu
+++ test/cuda/ut_lambda_tensor.cu
@@ -10,30 +10,6 @@ struct test_op {
     int __device__ __host__ operator()(pointi<2> idx) const { return 3; }
 };
 
-__host__ int geth() { return 2; }
-
-__device__ int getd() { return 3; }
-
-__device__ __host__ int gethd() { return 4; }
-
-#pragma nv_exec_check_disable
-__host__ void gethtest() {
-    geth();
-    getd();
-}
-
-#pragma nv_exe_check_disable
-__device__ void getdtest() {
-    gethd();
-    getd();
-}
-
-#pragma nv_exec_check_disable
-__host__ __device__ void gethdtest() {
-    geth();
-    getd();
-}
-
 TEST(MakeCudaLambdaTensor, TestMakeByLambda) {
     auto clt_test1 = cuda::make_lambda(pointi<2>{1000, 1000},
                                        [] __matazure__(pointi<2> idx) -> int { return idx[0]; })
