81fb60d9862bad5058c6202c464cf11735c41aca
Zhang Zhimin <p3.1415@qq.com>
2020-04-17 13:02:57 +0800

    add point4f copy cuda

--- benchmark/cuda/bm_algorithm.cu
+++ benchmark/cuda/bm_algorithm.cu
@@ -5,6 +5,8 @@ auto bm_cuda_tensor1f_copy = bm_tensor_copy<cuda::tensor<float, 1>>;
 auto bm_cuda_tensor2f_copy = bm_tensor_copy<cuda::tensor<float, 2>>;
 BENCHMARK(bm_cuda_tensor1f_copy)->Arg(1_G);
 BENCHMARK(bm_cuda_tensor2f_copy)->Arg(32_K);
+auto bm_cuda_tensor2p4f_copy = bm_tensor_copy<cuda::tensor<point4f, 2>>;
+BENCHMARK(bm_cuda_tensor2p4f_copy)->Arg(8_K);
 
 auto bm_cuda_tensor2f_fill = bm_tensor_fill<cuda::tensor<float, 2>>;
 BENCHMARK(bm_cuda_tensor2f_fill)->Arg(32_K);
