a835340ef0b772f8b0c4a90d65bdbae54f22474e
Zhang Zhimin <p3.1415@qq.com>
2020-05-14 21:52:20 +0800

    add point reverse ut

--- test/ut_point.hpp
+++ test/ut_point.hpp
@@ -11,3 +11,10 @@ TEST(PointTests, AssignmentOperator) {
     tmp /= tmp;
     tmp %= tmp;
 }
+
+TEST(PointTests, Reverse) {
+    pointi<4> pt{0, 1, 2, 3};
+    auto pt_re = reverse(pt);
+    pointi<4> pt_gold{3, 2, 1, 0};
+    EXPECT_TRUE(equal(pt_gold, pt_re));
+}
