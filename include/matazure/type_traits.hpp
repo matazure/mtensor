623409b8892e782280fc675e3b7c7d492bf9bd38
Zhang Zhimin <p3.1415@qq.com>
2020-05-15 16:17:53 +0800

    refactor zip

--- include/matazure/type_traits.hpp
+++ include/matazure/type_traits.hpp
@@ -102,4 +102,67 @@ MATAZURE_DEFINE_NONE_TAG(none_device_memory, memory_type, device_tag)
 MATAZURE_DEFINE_NONE_TAG(none_are_linear_index, index_type, linear_index)
 MATAZURE_DEFINE_NONE_TAG(none_array_access, index_type, array_index)
 
+#ifdef __cpp_lib_integer_sequence
+//使用标准库的integer_sequence
+using std::integer_sequence;
+using std::make_integer_sequence;
+#else
+
+template <typename _T, _T... Indices>
+struct integer_sequence;
+
+template <>
+struct integer_sequence<int_t, 0> {
+    typedef int_t value_type;
+
+    static constexpr std::size_t size() noexcept { return 1; }
+};
+
+template <>
+struct integer_sequence<int_t, 0, 1> {
+    typedef int_t value_type;
+
+    static constexpr std::size_t size() noexcept { return 2; }
+};
+
+template <>
+struct integer_sequence<int_t, 0, 1, 2> {
+    typedef int_t value_type;
+
+    static constexpr std::size_t size() noexcept { return 3; }
+};
+
+template <>
+struct integer_sequence<int_t, 0, 1, 2, 3> {
+    typedef int_t value_type;
+
+    static constexpr std::size_t size() noexcept { return 4; }
+};
+
+template <typename _T, _T _N>
+struct make_integer_sequence_helper;
+
+template <>
+struct make_integer_sequence_helper<int_t, 1> {
+    typedef integer_sequence<int_t, 0> type;
+};
+
+template <>
+struct make_integer_sequence_helper<int_t, 2> {
+    typedef integer_sequence<int_t, 0, 1> type;
+};
+template <>
+struct make_integer_sequence_helper<int_t, 3> {
+    typedef integer_sequence<int_t, 0, 1, 2> type;
+};
+template <>
+struct make_integer_sequence_helper<int_t, 4> {
+    typedef integer_sequence<int_t, 0, 1, 2, 3> type;
+};
+
+template <typename _T, _T _N>
+using make_integer_sequence = typename make_integer_sequence_helper<_T, _N>::type;
+
+#endif
+
 }  // namespace matazure
--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,7 @@
 #include "ut_tensor.hpp"
 #include "view/ut_permute.hpp"
 #include "view/ut_slice.hpp"
+#include "view/ut_zip.hpp"
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
