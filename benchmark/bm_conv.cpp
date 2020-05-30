5c435306284f02471bd67e2553f7cce13eac2b0f
Zhang Zhimin <p3.1415@qq.com>
2020-05-30 10:06:13 +0800

    use avs in x86

--- benchmark/bm_conv.cpp
+++ benchmark/bm_conv.cpp
@@ -22,8 +22,11 @@ BENCHMARK(bm_host_tensor2_view_conv_stride2_relu6_local_tensor3x3)->Arg(128)->Ar
 // BENCHMARK(bm_host_tensor2_view_conv_neighbors_weights3x3)->Arg(128)->Arg(10_K);
 
 #ifdef __GNUC__
+#ifdef __AVX__
+typedef float simd_type __attribute__((vector_size(32)));
+#else
 typedef float simd_type __attribute__((vector_size(16)));
-
+#endif
 auto bm_host_tensor2simd_view_conv_local_tensor3x3 =
     bm_tensor2_view_conv_local_tensor3x3<tensor<simd_type, 2>>;
 BENCHMARK(bm_host_tensor2simd_view_conv_local_tensor3x3)->Arg(128)->Arg(2_K);
